f7a5ecf237a2d9bd8ff4539846a04536b2d30b16
shubham sarode <shubham.sarode@extwebtech.in>
2024-06-22 10:43:08 +0530

    added the find range prime and prime pair problem

--- Interview_preparationkit/findAPrimeNumberInRange.ts
+++ Interview_preparationkit/findAPrimeNumberInRange.ts
@@ -1,30 +1,23 @@
-function isPrime(num: number) {
-    if (num <= 1) return false;
+// find the prime in range
 
-    
-    for (let i = 2; i * i <= num; i += 6) {
-        if (num % i === 0 || num % (i + 2) === 0) return false;
+function findPrimesInRange(L: number, R: number) {
+    const start=L,num=R;
+         let arr = [];
+  for(let j = start; j <= num; j++) {
+    let isPrime = true;
+    if (j < 2) { 
+      isPrime = false;
     }
-    
-    return true;
-}
-
-function findPrimesInRange(start: number, end: number) {
-    let primes = [];
-    for (let i = start; i <= end; i++) {
-        if (isPrime(i)) {
-            primes.push(i);
-        }
+    const s = Math.sqrt(j); 
+    for(let i = 2; i <= s; i++) { 
+      if(j % i === 0) {
+        isPrime = false;
+        break;
+      }
     }
-    return primes;
-}
-
-let start = 10;
-let end = 100;
-
-if (isNaN(start) || isNaN(end) || start > end) {
-    console.log("Invalid range. Please enter valid numbers.");
-} else {
-    let primes = findPrimesInRange(start, end);
-    console.log("Prime numbers in range:", primes);
+    if(isPrime) {
+      arr.push(j);
+    }
+  }
+  return arr;
 }
