f84e035de5394f77ac6fc544fe1f6220518b5f17
shubham sarode <shubham.sarode@extwebtech.in>
2024-07-22 17:33:39 +0530

    solve geek for geek union problem

---
+++ Interview_preparationkit/Remove_all_occurence_of_duplicates_in_a_linked_list.ts
@@ -0,0 +1,18 @@
+/*
+Remove all occurences of duplicates in a linked list
+Given a sorted linked list, delete all nodes that have duplicate numbers 
+(all occurrences), leaving only numbers that appear once in the original list,
+ and return the head of the modified linked list. 
+ 23->28->28->35->49->49->53->53
+
+Input: Linked List = 23->28->28->35->49->49->53->53
+Output: 23 35
+*/
+
+
+
+class Solution {
+    removeAllDuplicates(node) {
+        // your code here
+    }
+}
---
+++ Interview_preparationkit/union_intersection.ts
@@ -0,0 +1,25 @@
+// find the union and intersectoion
+
+
+const arr = [1, 3, 4, 5, 7]
+const arr2= [2, 3, 5, 6]
+const intersection = []
+for(let i = 0; i<arr.length; i++){
+    if(!arr2.includes(arr[i])){
+        arr2.push(arr[i])
+    }else{
+         intersection.push(arr[i])
+    }
+}
+
+for(let i = 0;i<arr2.length;i++ ){
+    for(let j =0;j<arr2.length;j++){
+        if(arr2[i]<arr2[j]){
+            let temp = arr2[i];
+            arr2[i]= arr2[j];
+            arr2[j] = temp;
+            
+        }
+    }
+}
+console.log(arr2,{intersection})
